846dd44801d7d79e06c668b44b78052f2ce206a7
Pavel Naumenko <pavelnaumenkowork@gmail.com>
2024-11-08 14:00:12 +0300

    added br0

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -3,7 +3,7 @@ virt-install \
 --ram 4096 \
 --vcpus 2 \
 --disk size=15,bus=virtio \
---network bridge=virbr0,model=virtio \
+--network bridge=br0,model=virtio \
 --graphics none \
 --location /opt/images/ubuntu-24.04.1-live-server-amd64.iso \
 --extra-args="console=ttyS0,115200n8 autoinstall ds=file:///home/devops/virsh-install/preseed_ubuntu.cfg" \
