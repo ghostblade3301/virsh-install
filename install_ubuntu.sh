a128c207991627891bae7084824e5abc7bb55eeb
Pavel Naumenko <pavelnaumenkowork@gmail.com>
2024-11-08 13:37:49 +0300

    deleted deprecated lines

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -2,12 +2,10 @@ virt-install \
 --name ubuntu-server \
 --ram 2048 \
 --vcpus 2 \
---os-type linux \
 --disk size=15,bus=virtio \
 --network bridge=br0,model=virtio \
 --graphics none \
 --location /opt/images/ubuntu-24.04.1-live-server-amd64.iso \
---metadata 'autoinstall_ds="file:///home/devops/virsh-install/preseed_ubuntu.txt"' \
---extra-args="console=ttyS0,115200n8 autoinstall ds=${autoinstall_ds}" \
+--extra-args="console=ttyS0,115200n8 autoinstall ds=file:///home/devops/virsh-install/preseed_ubuntu.cfg" \
 --initrd-inject /home/gh/devops/virsh-install/preseed.cfg \
 --noautoconsole
