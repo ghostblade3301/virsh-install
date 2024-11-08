0d42e057eff824bf7d23e10284322030aa5341a1
Pavel Naumenko <pavelnaumenkowork@gmail.com>
2024-11-08 10:41:16 +0000

    fixed filename

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -7,5 +7,5 @@ virt-install \
 --graphics none \
 --location /opt/images/ubuntu-24.04.1-live-server-amd64.iso \
 --extra-args="console=ttyS0,115200n8 autoinstall ds=file:///home/devops/virsh-install/preseed_ubuntu.cfg" \
---initrd-inject /home/gh/devops/virsh-install/preseed.cfg \
+--initrd-inject /home/gh/devops/virsh-install/preseed_ubuntu.cfg \
 --noautoconsole
